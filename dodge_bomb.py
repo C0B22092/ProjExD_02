f65e3fcb59a774861e907e4fbd467a56c9a25a25
fish <c0b22092ca@edu.teu.ac.jp>
2023-04-25 15:49:23 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,6 +10,20 @@ delta = {
     pg.K_RIGHT: (+1, 0),
     }
 
+def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し、真理値(True,False)タプルを返す関数
+    引数1:画面SurfaceのRect
+    引数2:こうかとん,または,爆弾SurfaceのRect
+    戻り値:横方向,縦方向のはみ出し判定結果(画面内:True/画面外/False)
+    """
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -18,6 +32,8 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
+
     bb_img = pg.Surface((20, 20))  # 正方形surface作成
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤い円を描画
     bb_img.set_colorkey((0, 0, 0))  # 黒を透過
@@ -38,10 +54,19 @@ def main():
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
+        if check_bound(screen.get_rect(), kk_rct) != (True, True):
+            for k, mv in delta.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0], -mv[1])
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(screen.get_rect(), bb_rct)
+        if not yoko:  # 横方向にはみ出ていたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出ていたら
+            vy *= -1
         screen.blit(bb_img, bb_rct)
 
         pg.display.update()
